fc8ca547e6f99381da9926f8f9b2b4c628c9c9a5
wanijonathanmg-lang <wanijonathanmg@gmail.com>
2026-06-01 16:12:45 +0530

    Add files via upload

---
+++ list.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo " <ol> <li> fruits</li>";
+        echo "<li>countries</li>";
+        echo "<li>animals</li>";
+        echo "<li>schools</li>";
+        echo "<li>birds</li>";
+        echo "<li>winter</li>";
+        echo "<li>colors</li>";
+        echo "<li>bottles</li>";
+        echo "<li>stars</li>";
+        echo "<li>sports</li>";
+        echo "<li>names</li>";
+        echo "<li>train</li>";
+        echo "<li>waterfalls</li>";
+        echo "<li>computer science</li>";
+        echo "<li>bags</li>";
+        echo "<li>friends</li></ol>";
+    ?>
+</body>
+</html>
